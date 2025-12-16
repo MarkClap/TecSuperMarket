7272b0afe2440ead504cadce45709f9e1c558a42
MarkClap <blusito48@gmail.com>
2025-12-15 23:50:32 -0500

    add protect route

--- TecSupermarket/src/main/java/TecSupermarket/controller/AuthController.java
+++ TecSupermarket/src/main/java/TecSupermarket/controller/AuthController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -48,6 +49,7 @@ public class AuthController {
     }
 
     @GetMapping("/check-auth")
+    @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
     public ResponseEntity<String> checkAuth(){
         return ResponseEntity.ok().body("Autenticado");
     }
